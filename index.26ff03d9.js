4b00f0254f00db8d06774b76810f0e06defba0bc
AnnaofAmber <126106997+AnnaofAmber@users.noreply.github.com>
2023-09-09 19:22:04 +0000

    Deploying to gh-pages from @ AnnaofAmber/password-generator-app@40ff70f5d0780f89a35a534b0aa719ca3f293ba3 🚀

--- index.26ff03d9.js
+++
@@ -1,9 +0,0 @@
-const e=document.querySelector(".input-length"),t=document.querySelector(".number-length");let n="rtl"===document.documentElement.dir;e.addEventListener("input",function(e){let r=e.target;"range"!==e.target.type&&(r=document.getElementById("character-length"));let c=r.min,o=r.max,u=r.value;t.textContent=u;let d=(u-c)*100/(o-c);n&&(d=o-u),r.style.backgroundSize=d+"% 100%"});const r=new MutationObserver(// Handle element change, check for dir attribute value change
-function(e,t){e.forEach(function(e){"attributes"===e.type&&"dir"===e.attributeName&&(n="rtl"===e.target.dir)})});r.observe(document.documentElement,{attributes:!0});/**
-   |============================
-   |dark theme
-   |============================
- */const c=document.querySelector(".body"),o=document.getElementById("toggle");o.addEventListener("change",function(e){e.currentTarget.checked?c.classList.add("body-ligth"):c.className="";// body.className = "";
-});//# sourceMappingURL=index.26ff03d9.js.map
-
-//# sourceMappingURL=index.26ff03d9.js.map
--- index.74e02857.js
+++
@@ -1,9 +0,0 @@
-!function(){let e=document.querySelector(".input-length"),t=document.querySelector(".number-length"),n="rtl"===document.documentElement.dir;e.addEventListener("input",function(e){let r=e.target;"range"!==e.target.type&&(r=document.getElementById("character-length"));let c=r.min,u=r.max,o=r.value;t.textContent=o;let d=(o-c)*100/(u-c);n&&(d=u-o),r.style.backgroundSize=d+"% 100%"});let r=new MutationObserver(// Handle element change, check for dir attribute value change
-function(e,t){e.forEach(function(e){"attributes"===e.type&&"dir"===e.attributeName&&(n="rtl"===e.target.dir)})});r.observe(document.documentElement,{attributes:!0});/**
-   |============================
-   |dark theme
-   |============================
- */let c=document.querySelector(".body"),u=document.getElementById("toggle");u.addEventListener("change",function(e){e.currentTarget.checked?c.classList.add("body-ligth"):c.className="";// body.className = "";
-})}();//# sourceMappingURL=index.74e02857.js.map
-
-//# sourceMappingURL=index.74e02857.js.map

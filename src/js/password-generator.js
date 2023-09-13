3a9a1af9233f29be83d57460d5279c6a455bffec
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-13 20:04:40 +0300

    added check for empty checkboxes

--- src/js/password-generator.js
+++ src/js/password-generator.js
@@ -1,11 +1,11 @@
-import { strengthCheck } from "./strength-check";
-
+import { strengthCheck } from './strength-check';
 
 const password = document.querySelector('.text-password');
 const btnGenerate = document.querySelector('.btn-generate');
 const passwordLength = document.querySelector('.number-length');
-const copied = document.querySelector('.text-copied')
-const strengthLevel = document.querySelector('.strength-level ')
+const copied = document.querySelector('.text-copied');
+const strengthLevel = document.querySelector('.strength-level ');
+const strength = document.querySelector('.strength-value')
 
 const checkboxes = {
   uppercase: document.getElementById('uppercase'),
@@ -13,6 +13,18 @@ const checkboxes = {
   numbers: document.getElementById('numbers'),
   symbols: document.getElementById('symbols'),
 };
+
+const checkboxesValue = Object.values(checkboxes);
+
+checkboxesValue.forEach(el=>{
+    el.addEventListener('change', checkboxesCheck)
+})
+
+function checkboxesCheck(){
+if(checkboxesValue.some(el=> el.checked)){
+    checkboxesValue.forEach(el=>el.classList.remove('input-check-error'))
+}}
+
 
 
 let charsUppercase = [
@@ -76,75 +88,83 @@ let charsNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let charsSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
-
 let chars = [];
 
+
+
 function onGenerate(e) {
-    strengthLevel.classList.remove('strength-level-weak')
-    strengthLevel.classList.remove('strength-level-too-weak')
-    strengthLevel.classList.remove('strength-level-medium')
-    strengthLevel.classList.remove('strength-level-strong')
-    let generatedPassword = '';
-    copied.textContent = ''
+  strengthLevel.classList.remove('strength-level-weak');
+  strengthLevel.classList.remove('strength-level-too-weak');
+  strengthLevel.classList.remove('strength-level-medium');
+  strengthLevel.classList.remove('strength-level-strong');
+  let generatedPassword = '';
+  copied.textContent = '';
   const selectedLength = Number(passwordLength.textContent);
   e.preventDefault();
 
-  if (checkboxes.uppercase.checked) {
-      if (!chars.includes(charsUppercase)){ chars.push(charsUppercase)}
-  }
-  else {
-        chars.splice(chars.indexOf(charsUppercase), 1)
-  }
-    if (checkboxes.lowercase.checked) {
-        if (!chars.includes(charsLowercase)) {
-            chars.push(charsLowercase);
-        }
+ 
+    if (checkboxesValue.every(el => !el.checked)) {
+        
+        strengthLevel.classList.add('strength-level-too-weak');
+        password.textContent = 'P4$5W0rD!';
+        strength.textContent = 'too weak'
+
+        checkboxesValue.forEach(el=>el.classList.add('input-check-error'))
     }
     else {
-        if (chars.includes(charsLowercase)) {
-            chars.splice(chars.indexOf(charsLowercase), 1)
+  
+        if (checkboxes.uppercase.checked) {
+            if (!chars.includes(charsUppercase)) {
+                chars.push(charsUppercase);
+            }
+        } else {
+            chars.splice(chars.indexOf(charsUppercase), 1);
         }
-    }
-
-
-    if (checkboxes.numbers.checked) {
-        if (!chars.includes(charsNumbers)) {
-            chars.push(charsNumbers)
+        if (checkboxes.lowercase.checked) {
+            if (!chars.includes(charsLowercase)) {
+                chars.push(charsLowercase);
+            }
+        } else {
+            if (chars.includes(charsLowercase)) {
+                chars.splice(chars.indexOf(charsLowercase), 1);
+            }
         }
-    }
-    else {
-        if (chars.includes(charsNumbers)) {
-            chars.splice(chars.indexOf(charsNumbers), 1)
+
+        if (checkboxes.numbers.checked) {
+            if (!chars.includes(charsNumbers)) {
+                chars.push(charsNumbers);
+            }
+        } else {
+            if (chars.includes(charsNumbers)) {
+                chars.splice(chars.indexOf(charsNumbers), 1);
+            }
         }
-    }
 
-
-    if (checkboxes.symbols.checked) {
-        if (!chars.includes(charsSymbols)) {
-            chars.push(charsSymbols)
+        if (checkboxes.symbols.checked) {
+            if (!chars.includes(charsSymbols)) {
+                chars.push(charsSymbols);
+            }
+        } else {
+            if (chars.includes(charsSymbols)) {
+                chars.splice(chars.indexOf(charsSymbols), 1);
+            }
         }
-    }
-    else {
-        if (chars.includes(charsSymbols)) {
-            chars.splice(chars.indexOf(charsSymbols), 1)
+
+        const selectedChars = chars.flatMap(el => {
+            return el;
+        });
+
+        for (let i = 0; i < selectedLength; i++) {
+            const selected = [];
+            selectedChars.forEach(el => selected.push(el));
+
+            let randomNumber = Math.floor(Math.random() * selected.length);
+            generatedPassword += selected.splice(randomNumber, 1);
+            password.textContent = generatedPassword;
         }
+
+        strengthCheck();
     }
-
-    const selectedChars = chars.flatMap(el => {
-return el
-    })
-
-    for (let i = 0; i < selectedLength; i++) {
-            const selected = []
-    selectedChars.forEach(el=> selected.push(el))
-
-    let randomNumber = Math.floor(Math.random() * selected.length);
-    generatedPassword += selected.splice(randomNumber, 1);
-    password.textContent = generatedPassword;
-  }
-
-strengthCheck()
 }
 
 btnGenerate.addEventListener('click', onGenerate);
-
